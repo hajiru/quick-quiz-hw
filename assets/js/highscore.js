b8e69784be17ec74b0533caaf4c738180547f15b
dabyn <davyn.inthavong@gmail.com>
2023-04-15 20:51:51 -0500

    displayed the scores in a list on highscore page

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,10 +1,9 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-console.log(
-    highScores
-        .map((score) => {
-            return `<li class="high-score">${score.name}-${score.score}</li>`;
-        })
-        .join("")
-);
+// Displays the scores in local storage in a list //
+highScoresList.innerHTML = highScores
+  .map((score) => {
+    return `<li class="high-score">${score.name}-${score.score}</li>`;
+  })
+  .join("");
